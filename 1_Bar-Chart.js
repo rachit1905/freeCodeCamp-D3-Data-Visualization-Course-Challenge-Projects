ba1047e2021389f6b2ae54caa3190a974a8c4cab
rachit1905 <96972170+rachit1905@users.noreply.github.com>
2023-03-28 14:35:40 +0530

    changes

--- 1_Bar-Chart.js
+++ 1_Bar-Chart.js
@@ -14,11 +14,12 @@ async function renderGraph() {
   const yScale = d3
     .scaleLinear()
     .domain([0, d3.max(data.data, (d) => d[1])])
-    .range([0, window.visualViewport.height]);
+    .range([20, window.visualViewport.height - 20]);
   const xScale = d3
     .scaleLinear()
     .domain([0, data.data.length - 1])
-    .range([40, window.visualViewport.width-44]);
+    // .domain([1947, 2015])
+    .range([40, window.visualViewport.width - 40]);
 
   const svg = d3
     .select("body")
@@ -32,27 +33,35 @@ async function renderGraph() {
     .data(data.data)
     .enter()
     .append("rect")
-    .style("background-color", "blue")
+    .attr("class", "bar")
+    // .style("background-color", "blue")
     .style("width", barWidth)
     .style("height", (d) => yScale(d[1]))
     .attr("x", (d, i) => xScale(i))
     .attr("y", (d) => {
-      return window.visualViewport.height - yScale(d[1] - 10);
-    });
+      return window.visualViewport.height - yScale(d[1]) - 26;
+    })
+    .append("title")
+    .text((d) => d[0]);
+  svg
+    .selectAll("rect")
+    .append("div")
+    .attr("id", "tooltip")
+    .text((d) => d[0]);
 
   // const yAxis = d3.axisLeft(yScale);
   // const xAxis = d3.axisBottom(xScale);
   const xAxis = d3.axisBottom(
     d3
       .scaleLinear()
-      .range([0, window.visualViewport.width])
+      .range([40, window.visualViewport.width - 20])
       .domain([1947, 2015])
   );
   const yAxis = d3.axisLeft(
     d3
-    .scaleLinear()
-    .domain([d3.max(data.data, (d) => d[1]), 0])
-    .range([10, window.visualViewport.height])
+      .scaleLinear()
+      .domain([d3.max(data.data, (d) => d[1]), 0])
+      .range([20, window.visualViewport.height - 20])
   );
   svg.append("g").attr("class", "translate-x-10").call(yAxis);
   svg.append("g").attr("class", "translate-y-10").call(xAxis);
